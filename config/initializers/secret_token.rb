e84d10dd110ca4b6dddc6d04b1ac82bfb2425fd5
schwegler <schwglr@me.com>
2012-10-08 10:32:49 -0400

    Initial Setup
    Using Rails, Thin, and ActiveAdmin as a base.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SGRITA::Application.config.secret_token = 'a1bed94125557c538115377866802298253941ef102004f8b644d21a008c6651e00f76be8480cfc232c6abf66c016c361bac6c5620a97e8201d2080becda21c5'
